a0a128fb0777252313908d6b5e268c2a6b1a5f39
tamasha <tamasha1210@gmail.com>
2026-07-13 15:28:18 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'nooooor-v5';
+const FILES = [
+  'index.html','home.html','products.html','club.html',
+  'entertainment.html','admin.html','contact.html','about.html',
+  'partner.html','manifest.json','logo.png',
+  'icon-192.png','icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return fetch(e.request).then(res => {
+        if(res && res.status === 200){
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached || caches.match('home.html'));
+    })
+  );
+});
